469c3a3fa7c991eaac250984ad90c1c61891ce07
Mahmoud Abdo <19627123+vipmaa1@users.noreply.github.com>
2020-07-06 22:34:00 +0200

    Initialize docker

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        //duplicate product for data 
+        $this->call(ProductsTableSeeder::class);
+    }
+}
